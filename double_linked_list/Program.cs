ff51c90934ea65625849a659519c1f6284493151
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-11-18 17:16:20 +0700

    Add Method Node previous and current

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -42,7 +42,26 @@ namespace double_linked_list
                     Console.WriteLine("\nDuplicate number not allowed");
                     return;
                 }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.next = null;
+                START = newNode;
+                return;
             }
+            /*if the node is to be inserted at beetwen two Node*/
+            Node previous, current;
+            for(current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+
         }
     }
 }
