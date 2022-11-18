41a69b556ad570dfa6be9699fb5ca9c5448dd563
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-11-18 16:50:13 +0700

    Add public integer, the name is noMhs in class Node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace double_linked_list
+{
+    class Node
+    {
+        /*Node class represent the node of doubly linked list
+          * It consists of the information part and links to
+          * its succeding and preceeding
+          * in terms of next and previous */
+        public int noMhs;
+    }
+}
