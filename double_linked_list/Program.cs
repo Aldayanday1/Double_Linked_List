15f6bd44371d218af32ab79602faf62cc81fd53d
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-11-18 20:09:55 +0700

    Add Switch Case 1

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -180,6 +180,14 @@ namespace double_linked_list
                     Console.WriteLine("6. Exit\n");
                     Console.WriteLine("Enter your choice (1-6): ");
                     char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                    }
                 }
             }
         }
